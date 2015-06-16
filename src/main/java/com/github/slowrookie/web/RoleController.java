ffce7e172e053fe69f003d508c3c15455138b0dd
slowrookie <liujiaxingemail@gmail.com>
2015-06-16 14:25:23 +0800

    update

---
+++ src/main/java/com/github/slowrookie/web/RoleController.java
@@ -0,0 +1,16 @@
+package com.github.slowrookie.web;
+
+import com.github.slowrookie.persistence.AuditablePersistable;
+
+/**
+ * 权限管理
+ * 
+ * @author 刘佳兴
+ */
+public class RoleController extends AuditablePersistable {
+	
+	private static final long serialVersionUID = -4215348297051867464L;
+
+	
+	
+}
