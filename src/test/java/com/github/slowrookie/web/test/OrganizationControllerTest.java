44a82bcd2abb914f75b74a1b73e653dddb9e2baf
liujiaxingemail@gmail.com <liujiaxingemail@gmail.com>
2015-06-26 08:40:15 +0800

    上班之前，来一发...

---
+++ src/test/java/com/github/slowrookie/web/test/OrganizationControllerTest.java
@@ -0,0 +1,71 @@
+package com.github.slowrookie.web.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.springframework.http.HttpMethod;
+
+import com.github.slowrookie.test.AbstractTest;
+
+/**
+ * 测试MenuController
+ * 
+ * @author 刘佳兴
+ */
+public class OrganizationControllerTest extends AbstractTest{
+
+	@Test
+	public void getMenus() throws Exception {
+		String url = HOST + "menus?page=0&size=10&sort=createdBy";
+		doRequest(url, HttpMethod.GET, null);
+	}
+	
+	@Test
+	public void saveMenu(){
+		String url = HOST + "menus";
+		
+		Map<String, Object> menu = new HashMap<>();
+		menu.put("name", "一级菜单");
+		menu.put("resource", "/menus0");
+		menu.put("parent", null);
+		doRequest(url, HttpMethod.PUT, menu);
+	}
+	
+	@Test
+	public void deleteMenu(){
+		String url = HOST + "menu/1";
+		doRequest(url, HttpMethod.DELETE, null);
+	}
+	
+	@Test
+	public void findOne(){
+		String url = HOST + "menu/1";
+		doRequest(url, HttpMethod.GET, null);
+	}
+	
+	@Test
+	public void saveMenus(){
+		String url = HOST + "menus";
+		List<Object> menus = new ArrayList<>();
+		
+		Map<String, Object> menu1 = new HashMap<>();
+		menu1.put("name", "二级菜单");
+		menu1.put("resource", "/menus/1/1");
+		menu1.put("parent", 1);
+		
+		Map<String, Object> menu2 = new HashMap<>();
+		menu2.put("name", "二级菜单01");
+		menu2.put("resource", "/menus/1/2");
+		menu2.put("parent", 1); 
+		
+		menus.add(menu1);
+		menus.add(menu2);
+		
+		doRequest(url, HttpMethod.POST, menus);
+	}
+	
+	
+}
