4eee6fbf17a7ef906fbc144c0fcee3baf9df6cc6
slowrookie <liujiaxingemail@gmail.com>
2015-06-27 01:29:53 +0800

    仔细思考。。。

--- src/test/java/com/github/slowrookie/web/test/DepartmentControllerTest.java
+++ src/test/java/com/github/slowrookie/web/test/DepartmentControllerTest.java
@@ -11,60 +11,60 @@ import org.springframework.http.HttpMethod;
 import com.github.slowrookie.test.AbstractTest;
 
 /**
- * 测试MenuController
+ * 测试DepartmentController
  * 
  * @author 刘佳兴
  */
 public class DepartmentControllerTest extends AbstractTest{
 
 	@Test
-	public void getMenus() throws Exception {
-		String url = HOST + "menus?page=0&size=10&sort=createdBy";
+	public void get() throws Exception {
+		String url = HOST + "department?page=0&size=10&sort=createdBy";
 		doRequest(url, HttpMethod.GET, null);
 	}
 	
 	@Test
-	public void saveMenu(){
-		String url = HOST + "menus";
+	public void save(){
+		String url = HOST + "departments";
 		
-		Map<String, Object> menu = new HashMap<>();
-		menu.put("name", "一级菜单");
-		menu.put("resource", "/menus0");
-		menu.put("parent", null);
-		doRequest(url, HttpMethod.PUT, menu);
+		Map<String, Object> department = new HashMap<>();
+		department.put("name", "测试部门");
+		department.put("code", "001");
+		department.put("parent", null);
+		doRequest(url, HttpMethod.PUT, department);
 	}
 	
 	@Test
-	public void deleteMenu(){
-		String url = HOST + "menu/1";
+	public void delete(){
+		String url = HOST + "department/1";
 		doRequest(url, HttpMethod.DELETE, null);
 	}
 	
 	@Test
 	public void findOne(){
-		String url = HOST + "menu/1";
+		String url = HOST + "department/1";
 		doRequest(url, HttpMethod.GET, null);
 	}
 	
 	@Test
-	public void saveMenus(){
-		String url = HOST + "menus";
-		List<Object> menus = new ArrayList<>();
+	public void saveAll(){
+		String url = HOST + "departments";
+		List<Object> departments = new ArrayList<>();
 		
-		Map<String, Object> menu1 = new HashMap<>();
-		menu1.put("name", "二级菜单");
-		menu1.put("resource", "/menus/1/1");
-		menu1.put("parent", 1);
+		Map<String, Object> departments1 = new HashMap<>();
+		departments1.put("name", "二级部门");
+		departments1.put("code", "00101");
+		departments1.put("parent", 1);
 		
-		Map<String, Object> menu2 = new HashMap<>();
-		menu2.put("name", "二级菜单01");
-		menu2.put("resource", "/menus/1/2");
-		menu2.put("parent", 1); 
+		Map<String, Object> departments2 = new HashMap<>();
+		departments2.put("name", "二级部门01");
+		departments2.put("code", "00101");
+		departments2.put("parent", 1); 
 		
-		menus.add(menu1);
-		menus.add(menu2);
+		departments.add(departments1);
+		departments.add(departments2);
 		
-		doRequest(url, HttpMethod.POST, menus);
+		doRequest(url, HttpMethod.POST, departments);
 	}
 	
 	
--- src/test/java/com/github/slowrookie/web/test/OrganizationControllerTest.java
+++ src/test/java/com/github/slowrookie/web/test/OrganizationControllerTest.java
@@ -11,60 +11,60 @@ import org.springframework.http.HttpMethod;
 import com.github.slowrookie.test.AbstractTest;
 
 /**
- * 测试MenuController
+ * 测试OrganizationController
  * 
  * @author 刘佳兴
  */
 public class OrganizationControllerTest extends AbstractTest{
 
 	@Test
-	public void getMenus() throws Exception {
-		String url = HOST + "menus?page=0&size=10&sort=createdBy";
+	public void get() throws Exception {
+		String url = HOST + "organizations?page=0&size=10&sort=createdBy";
 		doRequest(url, HttpMethod.GET, null);
 	}
 	
 	@Test
-	public void saveMenu(){
-		String url = HOST + "menus";
+	public void save(){
+		String url = HOST + "organizations";
 		
 		Map<String, Object> menu = new HashMap<>();
-		menu.put("name", "一级菜单");
-		menu.put("resource", "/menus0");
+		menu.put("name", "一级组织");
+		menu.put("code", "001");
 		menu.put("parent", null);
 		doRequest(url, HttpMethod.PUT, menu);
 	}
 	
 	@Test
-	public void deleteMenu(){
-		String url = HOST + "menu/1";
+	public void delete(){
+		String url = HOST + "organization/1";
 		doRequest(url, HttpMethod.DELETE, null);
 	}
 	
 	@Test
 	public void findOne(){
-		String url = HOST + "menu/1";
+		String url = HOST + "organization/1";
 		doRequest(url, HttpMethod.GET, null);
 	}
 	
 	@Test
-	public void saveMenus(){
-		String url = HOST + "menus";
-		List<Object> menus = new ArrayList<>();
+	public void saveAll(){
+		String url = HOST + "organizations";
+		List<Object> organizations = new ArrayList<>();
 		
-		Map<String, Object> menu1 = new HashMap<>();
-		menu1.put("name", "二级菜单");
-		menu1.put("resource", "/menus/1/1");
-		menu1.put("parent", 1);
+		Map<String, Object> organization1 = new HashMap<>();
+		organization1.put("name", "二级组织");
+		organization1.put("code", "00101");
+		organization1.put("parent", 1);
 		
-		Map<String, Object> menu2 = new HashMap<>();
-		menu2.put("name", "二级菜单01");
-		menu2.put("resource", "/menus/1/2");
-		menu2.put("parent", 1); 
+		Map<String, Object> organization2 = new HashMap<>();
+		organization2.put("name", "二级组织01");
+		organization2.put("code", "00102");
+		organization2.put("parent", 1); 
 		
-		menus.add(menu1);
-		menus.add(menu2);
+		organizations.add(organization1);
+		organizations.add(organization2);
 		
-		doRequest(url, HttpMethod.POST, menus);
+		doRequest(url, HttpMethod.POST, organizations);
 	}
 	
 	
